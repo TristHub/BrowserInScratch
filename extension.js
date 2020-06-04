c028e600593dedf5b7d25afde3c0738632f03b1f
TristHub <57120755+TristHub@users.noreply.github.com>
2020-06-04 18:59:03 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -27,6 +27,52 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
+    
+    ext._openIframe = function(iframelink) {
+        var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: iframelink,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+    </script>
+  </body>
+</html>;
+    };
+
 
     var descriptor = {
         blocks: [
@@ -35,6 +81,8 @@
             ['r', 'ask %s', 'ask', 'How are you?'],
             [' ', 'set window title to %s', 'setTitle', 'title'],
             [' ', 'open tab with %s', 'openTab', 'https://twitter.com/scratchteam']
+            [' ', 'open %s in a iframe', 'openIframe', 'https://twitter.com/scratchteam']
+        ]
         ]
     };
 
